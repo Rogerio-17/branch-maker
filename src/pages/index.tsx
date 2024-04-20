a5bf7bc3ff62d448f3dd45c24d9f50e09f1f3681
Alex Alan Nunes <aalannunesdelima@apple.com>
2024-04-20 20:54:51 -0300

    add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRef, useState } from "react";
 
 const BRANCH_TYPES = ["bug", "task", "feat", "enh", "hotfix", "enhancement"];
@@ -40,37 +41,42 @@ export default function Home() {
   const result = value ? `${type}/${radarNumber}-${formatText}` : ":)";
 
   return (
-    <main className={`min-h-screen w-full max-w-[720px] mx-auto mt-10`}>
-      <h1 className="text-4xl font-medium text-center">Branch Maker</h1>
+    <>
+      <Head>
+        <title>Branch Maker</title>
+      </Head>
+      <main className={`min-h-screen w-full max-w-[720px] mx-auto mt-10`}>
+        <h1 className="text-4xl font-medium text-center">Branch Maker</h1>
 
-      <div className="flex flex-col gap-8 mt-10">
-        <input
-          type="text"
-          value={value}
-          onChange={(ev) => {
-            setValue(ev.target.value);
-          }}
-          placeholder="Past here..."
-          className="p-3 px-5 border-2 border-gray-200 focus:border-blue-300 rounded-lg focus:ring-2 focus:outline-none focus:ring-blue-300"
-        />
+        <div className="flex flex-col gap-8 mt-10">
+          <input
+            type="text"
+            value={value}
+            onChange={(ev) => {
+              setValue(ev.target.value);
+            }}
+            placeholder="Past here..."
+            className="p-3 px-5 border-2 border-gray-200 focus:border-blue-300 rounded-lg focus:ring-2 focus:outline-none focus:ring-blue-300"
+          />
 
-        <div
-          ref={divRef}
-          data-testid="result-box"
-          className="rounded-2xl bg-gray-50 p-5 font-medium border border-gray-100"
-        >
-          {result}
-        </div>
-        <div className="text-center">
-          <button
-            disabled={!value}
-            onClick={handleCopy}
-            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-md px-10 py-2.5 text-center cursor-pointer disabled:opacity-80 disabled:cursor-not-allowed"
+          <div
+            ref={divRef}
+            data-testid="result-box"
+            className="rounded-2xl bg-gray-50 p-5 font-medium border border-gray-100"
           >
-            Copy
-          </button>
+            {result}
+          </div>
+          <div className="text-center">
+            <button
+              disabled={!value}
+              onClick={handleCopy}
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-md px-10 py-2.5 text-center cursor-pointer disabled:opacity-80 disabled:cursor-not-allowed"
+            >
+              Copy
+            </button>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
